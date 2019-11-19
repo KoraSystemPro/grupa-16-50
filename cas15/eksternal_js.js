0b97e662f0689816153730c9fc0922b432b7bea8
Luka <lukakorica@gmail.com>
2019-11-19 19:45:46 +0100

    Cas 15/16

---
+++ cas15/eksternal_js.js
@@ -0,0 +1,6 @@
+function prvaFunkcija(){
+	document.getElementById("primer").innerHTML = "Ovo je sada promenjeni paragraf!";
+}
+function reset(){
+	document.getElementById("primer").innerHTML = "Ovo je primer paragrafa.";
+}
---
+++ cas16/skript.js
@@ -0,0 +1,31 @@
+function promeni(x){
+	document.getElementById("primer").innerHTML = "Promenjena vrednost paragrafa.";
+}
+
+// window.alert("Da li je sve u redu?!");
+
+/*
+console.log("Zbir 5 i 3 je: ")
+console.log(5 + 3);
+*/
+
+var x, y, z;
+x = 23;
+y = 3.2;
+z = x + y;
+
+console.log(z);
+
+var ime, prezime, pol;
+ime = "Luka";
+prezime = "Korica";
+pol = "m";
+
+/*
+var p = "5" + 2 + 3;
+console.log(p);
+*/
+
+
+
+window.alert("Doborodosao " + ime + " " + prezime + " " + z);
