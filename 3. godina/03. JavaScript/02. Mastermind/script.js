3d068ca0afc79e1830ee349e1f9b8af9299c448a
Luka <lukakorica@gmail.com>
2020-11-29 21:24:53 +0100

    Dodata nedelja 29.11.2020.

--- 3. godina/03. JavaScript/02. Mastermind/script.js
+++ 3. godina/03. JavaScript/02. Mastermind/script.js
@@ -1,9 +1,12 @@
 let kombinacija = [0, 0, 0, 0];
+let pokusaj = [6, 4, 4, 2];
+let niz_crnih = [0, 0, 0, 0];
+let prebrojavanje = [0, 0, 0, 0, 0, 0];
+                //   1  2  3  4  5  6
 let crni = 0;
 let beli = 0;
 
-function oceni(){
-
+function ispisi(){
     document.getElementById("kombinacija").innerHTML = "Kombinacija: ";
     for(i = 0; i < 4; i++){
         document.getElementById("kombinacija").innerHTML += kombinacija[i];
@@ -12,7 +15,38 @@ function oceni(){
     document.getElementById("beli").innerHTML = "Beli: " + beli;
 }
 
-document.getElementById("nova-kombinacija").addEventListener("click", function novaKombinacija(){
+function oceni(){
+    crni = 0;
+    beli = 0;
+    niz_crnih = [0, 0, 0, 0];
+    prebrojavanje = [0, 0, 0, 0, 0, 0];
+    // Kombinacija  3 4 4 6
+    // Pokusaj      6 4 4 2
+    
+    // Trazimo crni i potencijalno beli pogodak
+    for(i = 0; i < 4; i++){
+        if(pokusaj[i] == kombinacija[i]){
+            // Nasli crni broj
+            crni++;
+            niz_crnih[i] = 1;
+        } else {
+            // Nasli smo potencijalno beli broj
+            prebrojavanje[kombinacija[i] - 1]++;
+        }
+    }
+
+    // Trazimo beli pogodak
+    for(i = 0; i < 4; i++){
+        if((prebrojavanje[pokusaj[i] - 1]  > 0) && (niz_crnih[i] == 0)){
+            beli++;
+            prebrojavanje[pokusaj[i] - 1]--;
+        }
+    }
+
+    ispisi();
+}
+
+function novaKombinacija(){
     // console.log(Math.round(Math.random()*10000) % 6 + 1);
     for(i = 0; i < 4; i++){
         kombinacija[i] = Math.round(Math.random()*10000) % 6 + 1;
@@ -24,5 +58,6 @@ document.getElementById("nova-kombinacija").addEventListener("click", function n
     // 0 1 2 3 4 5  Ostaci za deljenje sa 6
 
     oceni();
-   
-});
+}
+
+document.getElementById("nova-kombinacija").addEventListener("click", novaKombinacija);
