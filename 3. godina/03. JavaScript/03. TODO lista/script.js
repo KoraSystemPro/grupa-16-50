a268e6e108ff54f63bb012f6cd625db1e63758c6
Luka <lukakorica@gmail.com>
2020-12-12 18:24:28 +0100

    Dodata subota 12.12.2020.

--- 3. godina/03. JavaScript/03. TODO lista/script.js
+++ 3. godina/03. JavaScript/03. TODO lista/script.js
@@ -1,12 +1,47 @@
-// Kreira novi clan liste kada se pritisne na dugme Dodaj
-function napraviNoviElement(){
-    let li = document.createElement("li");
-    // Dohvatam vrednost iz input boxa i pretvaram ga u textNode koji pridruzujem na li
-    let vrednostTextBoxa = document.createTextNode(document.getElementById("inputSadrzaj").value);
-    li.appendChild(vrednostTextBoxa);
-
-    document.getElementById("todo-lista").appendChild(li);
-
+// Dohvata element na koji se kliknulo (this), uzima njegov parent element i stavlja mu display svojstvo na "none"
+function sakrijiElement(){
+    let element = this.parentElement;
+    element.style.display = "none";
 }
 
+// Kreira novi clan liste kada se pritisne na dugme Dodaj
+function napraviNoviElement(){
+    let noviLi = document.createElement("li");
+    // Dohvatam vrednost iz input boxa i pretvaram ga u textNode koji pridruzujem na li
+    let vrednostTextBoxa = document.getElementById("inputSadrzaj").value;
+    let tekstCvor = document.createTextNode(vrednostTextBoxa);
+    noviLi.appendChild(tekstCvor);
+
+    // Proveravamo da li je tekstbox bi prazan
+    if(vrednostTextBoxa == ""){
+        alert("Morate da unesete neki tekst!");
+    }
+    else{
+        document.getElementById("todo-lista").appendChild(noviLi);
+    }
+    // Brisemo sadrzaj input-a kada ga dodamo na listu
+    document.getElementById("inputSadrzaj").value = "";
+    
+    // Pravimo x za skidanje sa liste i dodajemo ga na li
+    let dugmeZaSkidanjeSaListe = document.createElement("span");
+    dugmeZaSkidanjeSaListe.className = "zatvori";
+    dugmeZaSkidanjeSaListe.appendChild(document.createTextNode("\u2716"));
+    noviLi.appendChild(dugmeZaSkidanjeSaListe);
+    
+    // Za svaki clan liste pravimo EventListener na x koji ce ga skloniti sa liste
+    let iksici = document.getElementsByClassName("zatvori");
+    let brojIksica = iksici.length;
+    for(i = 0; i < brojIksica; i++){
+        iksici[i].addEventListener("click", sakrijiElement);
+    }
+    
+}
+
+// Dodaje novi element na listu
 document.getElementById("dgm-dodaj").addEventListener("click", napraviNoviElement);
+// "Zavrsava" pritisnuti element
+document.getElementById("todo-lista").addEventListener("click", function(evt){
+    if(evt.target.tagName === "LI"){
+        evt.target.classList.toggle("gotovo");
+    }
+});
