89108851cdb0ab8a63a53e5fa62d6e51513c0fa7
Luka Korica <lukakorica@gmail.com>
2021-09-18 18:38:47 +0200

    18-SEP-2021

--- 4. godina/01. Igra Zivota JS/igraZivota.js
+++ 4. godina/01. Igra Zivota JS/igraZivota.js
@@ -29,6 +29,39 @@ function crtaj(ctx, matrica) {
     }
 }
 
+function prebrojKomsije(matrica, x, y){
+    let br_komsija = 0;
+    for(let i = -1; i < 2 && x+i < redovi && x+i > -1; i++){
+        for(let j = -1; j < 2 && y+j < kolona && y+j > -1; j++){
+            br_komsija += matrica[x+i][y+j];
+        }
+    }
+    br_komsija = br_komsija - matrica[x][y];
+    return br_komsija;
+}
+
+function napraviSledecuGeneraciju(prethodna_gen){
+    let nova_gen = napravi2DMatricu(redovi, kolona);
+
+    for(let i = 0; i < redovi; i++){
+        for(let j = 0; j < kolona; j++){
+            // Brojimo komsije
+            let br_komsija = prebrojKomsije(prethodna_gen, j, i);
+            // Pom promenljiva radi lakseg citanja
+            let trenutna_celija = prethodna_gen[i][j];
+
+            if(trenutna_celija == 0 && br_komsija == 3){
+                nova_gen[i][j] = 1;
+            } else if (trenutna_celija == 1 && (br_komsija < 2 || br_komsija > 3)){
+                nova_gen[i][j] = 0;
+            } else {
+                nova_gen[i][j] = trenutna_celija;
+            }
+        }
+    }
+    return nova_gen;
+}
+
 function napravi2DMatricu(rows, cols) {
     let niz = new Array(rows);
     for (let i = 0; i < rows; i++) {
@@ -50,11 +83,12 @@ function main() {
     let canv, ctx = spremiPlatno()
     let igra = napravi2DMatricu(redovi, kolona);
     igra = generisiIgru(igra);
-
+    console.log(igra);
     crtaj(ctx, igra);
 
-
-
+    igra = napraviSledecuGeneraciju(igra);
+    crtaj(ctx, igra);
+    
 }
 
 document.addEventListener("DOMContentLoaded", main);
