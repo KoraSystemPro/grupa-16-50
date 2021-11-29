108ced8bf292e3fef9832a31300d4246950c64d9
Luka Korica <lukakorica@gmail.com>
2021-11-29 15:55:18 +0100

    28-NOV-2021

---
+++ 4. godina/03. PHP/01. Uvod/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+
+    <body>
+        <?php 
+            echo("Hello world!");
+
+            $promenljiva = 5;               // Celobrojna vrednost
+            echo("Vrednost promenljive je: $promenljiva <br>");
+
+            $promenljiva = 32.12;           // Decimalna proemnljiva
+            echo("Vrednost promenljive je: $promenljiva <br>");
+            
+            $promenljiva = 'A';             // Znak/char
+            echo("Vrednost promenljive je: $promenljiva <br>");
+            
+            $promenljiva = "Ovo je string"; // Niska/string
+            
+            echo("Vrednost promenljive je: $promenljiva <br>");
+            
+            $promenljiva = true;            // Bool
+            echo("Vrednost promenljive je: $promenljiva <br>");
+        ?>
+
+    </body>
+</html>
