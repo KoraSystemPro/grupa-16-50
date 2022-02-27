a39bc1df550e470d0764d0ce8a0744ad3c4cf92d
Luka Korica <lukakorica@gmail.com>
2022-02-27 17:04:20 +0100

    26-FEB-2022

--- 4. godina/03. PHP/05. Prodavnica/main/index.php
+++ 4. godina/03. PHP/05. Prodavnica/main/index.php
@@ -1,5 +1,6 @@
 <?php
 include "./funkcije/pravljenje_itema.php";
+include "./funkcije/server.php";
 ?>
 
 <!DOCTYPE html>
@@ -15,6 +16,45 @@ include "./funkcije/pravljenje_itema.php";
         <div class="zaglavlje">
             <h1 class="naslov">Dućan</h1>
         </div>
+        <div class="dodavanje">
+            <form action="./index.php" method="$_GET">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Ime proizvoda..." aria-label="Ime proizvoda..." aria-describedby="button-addon2">
+                    <select class="form-select">
+                        <!-- <option selected>Kategorija</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option> -->
+                        <?php
+                            $konekcija = otvori_konekciju("localhost", "root", "", "prodavnica");
+                            $rez = $konekcija->query("SELECT * FROM `Category`;");
+                            while($row = $rez->fetch_assoc()){
+                                echo '<option value="' . $row['ID'] . '">' . $row['Name'] . '</option>';
+                            }
+                            
+                            zatvori_konekciju($konekcija);
+                        ?>
+                    </select>
+                    <select class="form-select">
+                        <?php
+                            $konekcija = otvori_konekciju("localhost", "root", "", "prodavnica");
+                            $rez = $konekcija->query("SELECT * FROM `Provider`;");
+                            while($row = $rez->fetch_assoc()){
+                                echo '<option value="' . $row['ID'] . '">' . $row['Name'] . '</option>';
+                            }
+                            
+                            zatvori_konekciju($konekcija);
+                        ?>
+
+                    </select>
+
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Dodaj</button>
+
+                </div>
+
+            </form>
+        </div>
+
         <div class="pretraga">
             <form action="./index.php" method="get">
                 <div class="input-group mb-3">
@@ -25,8 +65,6 @@ include "./funkcije/pravljenje_itema.php";
         </div>
         <div class="rezultati">
             <?php 
-            
-
                 napravi_item("#", "Hleb", "Sava beli hleb", 64.99);
                 napravi_item("#", "Orbit mint", "Hladne osvezavajuce zvake", 59.99);
                 napravi_item("#", "Banana", "Banane iz Konga", 120.00);
